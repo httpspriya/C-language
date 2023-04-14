a2d625aadb018f6f558c8e5fcde7e0273050d503
Priyanshi Sondagar <122718112+httpspriya@users.noreply.github.com>
2023-04-14 23:53:48 +0530

    Create hello.c

---
+++ Basics/hello.c
@@ -0,0 +1,21 @@
+/*
+
+    NAME:Priyanshi Sondagar
+
+    PROG.:Basics
+
+*/
+
+#include <stdio.h>
+
+#include <conio.h>
+
+void main()
+
+{
+
+    printf("Hello world!!");   //prints hello world!!
+
+    getch();
+
+}
